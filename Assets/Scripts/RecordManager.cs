57cf0abc60d4e933f280b227b1b90ff0962bdb3f
정유빈 <yubeen.jeong@aiaracorp.com>
2018-10-08 06:14:23 +0900

    녹음 관리 추가

---
+++ Assets/Scripts/RecordManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecordManager : MonoBehaviour {
+    public Image progress;
+    AudioSource source;
+    private bool isPlaying;
+
+    // Use this for initialization
+    void Start () {
+        source = GetComponent<AudioSource>();
+	}
+
+    private void Update()
+    {
+        if(source.isPlaying)
+            progress.fillAmount = source.time / 15f;
+    }
+
+    public void Record(){
+        isPlaying = true;
+        source.clip = Microphone.Start(null, false, 15, 16600);
+        source.volume = 0;
+        source.Play();
+    }
+
+    public void Stop(){
+        isPlaying = false;
+        Microphone.End(null);
+        source.Stop();
+    }
+
+    public void Play(){
+        if (isPlaying) return;
+        source.volume = 1;
+        source.Play();
+    }
+}
