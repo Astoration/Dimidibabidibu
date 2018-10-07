d43acd79c3824de407632e08f5971ffac89b8c74
정유빈 <yubeen.jeong@aiaracorp.com>
2018-10-08 05:59:28 +0900

    인덱스 스케일 애니메이션 적용

--- Assets/Scripts/SelectManager.cs
+++ Assets/Scripts/SelectManager.cs
@@ -7,6 +7,7 @@ public class SelectManager : MonoBehaviour {
     public GameObject viewport, indexList;
     public GameObject contentItem, indexItem;
     public GameObject indexDot;
+    public RectTransform currentObject, preObject;
     public List<GameObject> indexObjects = new List<GameObject>();
     public int current = 0;
 
@@ -24,16 +25,23 @@ public class SelectManager : MonoBehaviour {
         var pos = indexDot.GetComponent<RectTransform>().anchoredPosition;
         pos.x = Camera.allCameras[1].WorldToScreenPoint(indexObjects[current].GetComponent<RectTransform>().position).x;
         indexDot.GetComponent<RectTransform>().anchoredPosition = pos;
+        currentObject = indexObjects[current].GetComponent<ThingObject>().image.rectTransform;
     }
 
     public void SetCurrent(GameObject index){
         current = GetComponent<MagneticScrollRect>().CurrentSelectedIndex;
+        preObject = currentObject;
+        currentObject = indexObjects[current].GetComponent<ThingObject>().image.rectTransform;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
         var pos = indexDot.GetComponent<RectTransform>().anchoredPosition;
         pos.x = Camera.allCameras[1].WorldToScreenPoint(indexObjects[current].GetComponent<RectTransform>().position).x;
         indexDot.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(indexDot.GetComponent<RectTransform>().anchoredPosition, pos, 0.1f);
-	}
+        currentObject.localScale = Vector3.Lerp(currentObject.localScale, Vector3.one * 0.7477477f * 1.5f, 0.1f);
+        if (preObject)
+            preObject.localScale = Vector3.Lerp(preObject.localScale, Vector3.one * 0.7477477f, 0.1f);
+
+    }
 }
