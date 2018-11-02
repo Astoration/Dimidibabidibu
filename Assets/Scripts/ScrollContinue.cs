2711d24158485b6f9d99066a26f11579f8e3d3d8
정유빈 <yubeen.jeong@aiaracorp.com>
2018-11-02 21:22:29 +0900

    아카이빙 연속 스크롤 추가

---
+++ Assets/Scripts/ScrollContinue.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MagneticScrollView;
+
+public class ScrollContinue : MonoBehaviour
+{
+    public MagneticScrollRect scroll;
+    public float time = 0f;
+    public bool tickOn = false;
+    public bool isLeft = true;
+    public bool enable = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enable){
+            TickScroll();
+        }
+    }
+
+    public void EnableTick(){
+        enable = true;
+    }
+
+    public void TickScroll() 
+    {
+        time += Time.deltaTime;
+        if(tickOn&&0.3f<time){
+            time = 0f;
+            Scroll();
+        }
+        if(2f<time&&!tickOn){
+            Scroll();
+            tickOn = true;
+            time = 0f;
+        }
+    }
+
+    public void Scroll(){
+        if (isLeft)
+        {
+            scroll.ScrollBack();
+        }
+        else
+        {
+            scroll.ScrollForward();
+        }
+    }
+
+    public void ResetTime(){
+        time = 0f;
+        enable = false;
+        tickOn = false;
+    }
+}
